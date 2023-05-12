4b18153c77633d411adcfb63f31b9784c2085a18
Gonzague de Poulpiquet <gdepoulpiquet@me.com>
2023-05-12 20:09:18 +0200

    maj des test: cours N°13

--- cours/src/App.svelte
+++ cours/src/App.svelte
@@ -1,14 +1,17 @@
 <script>
   import { getContext } from "svelte";
+  import Fiche from "./Fiche.svelte";
   import Counter from "./lib/Counter.svelte";
   import Decounter from "./lib/Decounter.svelte";
-  
+
   let logo = "logo de svelte";
   let taille = 200;
   let url = "https://www.google.com";
-  let urlImg1='https://images.unsplash.com/photo-1683802175943-c71b4bd66c54?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2286&q=80'
-  let urlImg2 ='https://images.unsplash.com/photo-1682685797208-c741d58c2eff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80'
-  let src= urlImg1
+  let urlImg1 =
+    "https://images.unsplash.com/photo-1683802175943-c71b4bd66c54?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2286&q=80";
+  let urlImg2 =
+    "https://images.unsplash.com/photo-1682685797208-c741d58c2eff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80";
+  let src = urlImg1;
   let lien = "Google";
   let compteur = 0;
   let cont = -2;
@@ -16,16 +19,20 @@
     compteur += step;
   }
   function changeImg() {
-    src =
-      src === urlImg1
-        ? urlImg2
-        : urlImg1
-        
+    src = src === urlImg1 ? urlImg2 : urlImg1;
   }
   let texte = "un beau texte à inverser";
-  function inverser(){
-    texte = texte.split("").reverse().join("")
+  function inverser() {
+    texte = texte.split("").reverse().join("");
   }
+
+  $: prenomV = prenom.toUpperCase();
+  function changePrenom() {
+    prenom = prenom === "Gonzague" ? "Dominique" : "Gonzague";
+  }
+  let prenom = "",
+    nom = "",
+    age = "";
 </script>
 
 <main>
@@ -35,14 +42,20 @@
   </div>
   <hr />
   <div class="container">
-    <h1><a href={url} target="-blank">{lien}</a></h1>
+    <h1><a href={url} target="-blank">{lien} et {prenomV.toLowerCase()}</a></h1>
     <button on:click={(e) => incrementer(e, cont)}
       >Par {cont} = {compteur}</button
     >
     <img {src} alt={logo} width={taille} on:click={changeImg} />
   </div>
   <hr />
-  <p class='texte'><button on:click={inverser}>Inverser</button>{texte}</p>
+  <p class="texte"><button on:click={inverser}>Inverser</button>{texte}</p>
+  <h1 on:click={changePrenom}>{prenomV}</h1>
+  <input type="text" placeholder="Prenom ?" id="prenom" bind:value={prenom} />
+  <input type="text" placeholder="Nom ?" id="nom" bind:value={nom} />
+  <input type="number" placeholder="Age ?" id="age" bind:value={age} />
+
+  <Fiche {nom} {prenomV} {age} />
   <p>
     Check out <a
       href="https://github.com/sveltejs/kit#readme"
@@ -59,10 +72,14 @@
     align-items: center;
   }
   img {
-    height:auto;
+    height: auto;
   }
-.texte{
-  color:yellow;
-  font-size: 2rem;
-}
+  .texte {
+    color: yellow;
+    font-size: 2rem;
+  }
+  h1:hover {
+    cursor: pointer;
+    color: red;
+  }
 </style>
---
+++ cours/src/Fiche.svelte
@@ -0,0 +1,20 @@
+<script>
+    /* reçoit les variables de App */
+    export let nom, prenomV,age
+</script>
+<div class='fiche'>
+<p> nom : {prenomV} {nom}</p>
+<p> Age : {age} ans</p>
+
+</div>
+<style>
+    .fiche{
+        width:25rem;
+        height:10rem;
+        margin :1rem;
+        padding:2rem;
+        background: lightgreen;
+        font-size: 1.6rem;
+        color:black;
+    }
+</style>
